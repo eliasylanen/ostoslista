046d8c68d5b979b602745690d5afd6f65f84d27a
Elias Ylänen <elias.ylanen@gmail.com>
2017-01-16 09:59:43 +0200

    AppBar left element

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const ActionButtonContainer = () => (
-  <div>
-    {!localStorage.getItem('profile') && <h2>Login</h2>}
-    {localStorage.getItem('profile') && location.pathname.includes('/lists') && <h2>Lists</h2>}
-    {localStorage.getItem('profile') && !location.pathname.includes('/lists') && <h2>Jotakin</h2>}
-  </div>
+import './ActionButtons.css';
+
+function logged(props) {
+  if (!localStorage.getItem('profile')) return <h2>Login</h2>;
+  return location.pathname.includes('/lists')
+  ? <div><img src={props.profile.picture} alt={props.profile.nickname} /> {props.profile.name} - Lists</div>
+  : <div><img src={props.profile.picture} alt={props.profile.nickname} /> {props.profile.name} - Jotakin</div>;
+}
+
+const ActionButtonContainer = (props) => (
+  logged(props.profile)
 );
 
 export default ActionButtonContainer;
--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,20 +3,20 @@ import FontIcon from 'material-ui/FontIcon';
 import React from 'react';
 import FloatingActionButtonExampleSimple from './FloatingButton'
 
-
 import ActionButtonContainer from './ActionButtons';
 
 /**
  * A simple example of `AppBar` with an icon on the right.
  * By default, the left icon is a navigation-menu.
  */
-const AppBarExampleIcon = () => (
+const AppBarContainer = (props) => (
   <AppBar
+    style={{verticalAlign: 'middle'}}
     title="Ostoslista"
     showMenuIconButton={false}
-    iconElementRight={<ActionButtonContainer />}
+    iconElementRight={<ActionButtonContainer profile={props.profile} />}
   />
 );
 
-export default AppBarExampleIcon;
+export default AppBarContainer;
 
