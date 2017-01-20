34e3aec8ecfe5f9d9d4b58eb546452f996a6bac1
Juuso Vuorinen <Juuso Vuorinen>
2017-01-20 10:32:33 +0200

    Ostoslistojen viimeistely

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -16,7 +16,7 @@ const styles = {
   },
   card: {
     marginTop: 14,
-  }
+  },
   tabs: {
     marginBottom: 13,
   }
@@ -54,10 +54,14 @@ export default class TabsExampleSwipeable extends React.Component {
         >
           <div>
             <h2 style={styles.headline}>Omat listat</h2>
-            <Card style={styles.card} />
+            <Card />
+            <Card />
+            <Card />
           </div>
           <div style={styles.slide}>
             <h2 style={styles.headline}>Vastaanotetut listat</h2>
+            <Card />
+            <Card />
             <Card />
           </div>
         </SwipeableViews>
