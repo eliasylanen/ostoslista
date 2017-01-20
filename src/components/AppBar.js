29bd886f744bb8c559d119e4587966447853e6f8
Elias Ylänen <elias.ylanen@gmail.com>
2017-01-20 09:39:34 +0200

    Remove top bar bottom-margin and shawod

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -9,7 +9,7 @@ import ActionButtonContainer from './ActionButtons';
  */
 const AppBarContainer = (props) => (
   <AppBar
-    style={{verticalAlign: 'middle'}}
+    style={{verticalAlign: 'middle', boxShadow: 'none'}}
     title="Ostoslista"
     showMenuIconButton={false}
     iconElementRight={<ActionButtonContainer profile={props.profile} />}
