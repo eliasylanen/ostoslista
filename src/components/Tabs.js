883a9ee6523a81d3cae87cca1856f257e5119d2e
Juuso Vuorinen <Juuso Vuorinen>
2017-01-20 09:51:17 +0200

    mita hittoo

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -2,19 +2,24 @@ import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
 // From https://github.com/oliviertassinari/react-swipeable-views
 import SwipeableViews from 'react-swipeable-views';
-// Onko tää ok
 import Card from './Card';
 
 const styles = {
   headline: {
     fontSize: 24,
     paddingTop: 16,
-    marginBottom: 12,
     fontWeight: 400,
   },
   slide: {
     padding: 10,
+    marginTop: 13,
   },
+  card: {
+    marginTop: 14,
+  }
+  tabs: {
+    marginBottom: 13,
+  }
 };
 
 export default class TabsExampleSwipeable extends React.Component {
@@ -38,6 +43,7 @@ export default class TabsExampleSwipeable extends React.Component {
         <Tabs
           onChange={this.handleChange}
           value={this.state.slideIndex}
+          style={styles.tabs}
         >
           <Tab label="Omat listat" value={0} />
           <Tab label="Vastaanotetut listat" value={1} />
@@ -48,7 +54,7 @@ export default class TabsExampleSwipeable extends React.Component {
         >
           <div>
             <h2 style={styles.headline}>Omat listat</h2>
-            <Card />
+            <Card style={styles.card} />
           </div>
           <div style={styles.slide}>
             <h2 style={styles.headline}>Vastaanotetut listat</h2>
