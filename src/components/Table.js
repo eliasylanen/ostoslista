4dcf3d35c5142547adc1eda886d0c9c414127a7f
Juuso Vuorinen <Juuso Vuorinen>
2017-01-23 15:19:09 +0200

    tablee taas vähä mälvätty

--- src/components/Table.js
+++ src/components/Table.js
@@ -10,19 +10,23 @@ const styles = {
     fontSize: 32,
     color: (244, 66, 86),
   },
-  tableRow: {
+  tableCell: {
+    fontSize: 14,
+  },
+  tableHeaderNarrow: {
     fontSize: 16,
   },
   tableCellNarrow: {
-    width: 28,
+    fontSize: 14,
+    width: 18,
   },
   icon: {
     color: '#616161',
   },
   incrementIcons: {
     color: '#424242',
-    width: 17,
-    height: 17,
+    width: 11,
+    height: 11,
   },
   propContainer: {
     width: 200,
@@ -60,23 +64,25 @@ export default class TableExampleComplex extends React.Component {
         <Table style={styles.Table} >
           <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
             <TableRow>
-              <TableHeaderColumn tooltip="Poista" style={styles.tableCellNarrow}>Poista</TableHeaderColumn>
-              <TableHeaderColumn tooltip="Tuote">Tuote</TableHeaderColumn>
-              <TableHeaderColumn tooltip="Määrä">Määrä</TableHeaderColumn>
-              <TableHeaderColumn tooltip="Yht.">Yht.</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Poista" style={styles.tableHeaderNarrow}>Poista</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Tuote" style={styles.tableHeaderNarrow}>Tuote</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Määrä" style={styles.tableHeaderNarrow}>Määrä</TableHeaderColumn>
+              <TableHeaderColumn tooltip="Yht." style={styles.tableHeaderNarrow}>Yht.</TableHeaderColumn>
             </TableRow>
           </TableHeader>
-          <TableBody displayRowCheckbox={false} styles={styles.Table} >
+          <TableBody displayRowCheckbox={false} >
             {tableData.map( (row, index) => (
-              <TableRow key={index}>
+              <TableRow 
+                key={index}
+              >
                 <TableRowColumn style={styles.tableCellNarrow}><ContentClear style={styles.icon}/></TableRowColumn>
-                <TableRowColumn>{row.product}</TableRowColumn>
-                <TableRowColumn style={{marginLeft: '2px'}}>
-                  <Remove style={styles.incrementIcons} />
+                <TableRowColumn style={styles.tableCell}>{row.product}</TableRowColumn>
+                <TableRowColumn style={styles.tableCell}>
                   {row.numberOfItems}
                   <Add style={styles.incrementIcons} />
+                  <Remove style={styles.incrementIcons} />
                 </TableRowColumn>
-                <TableRowColumn>{row.sum}</TableRowColumn>
+                <TableRowColumn style={styles.tableCell}>{row.sum}</TableRowColumn>
               </TableRow>
               ))}
           </TableBody>
