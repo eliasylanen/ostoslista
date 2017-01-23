d8833c38578777359b1c1a960f9826839105ebea
Susanna Kiintonen <Susanna Kiintonen>
2017-01-23 15:17:03 +0200

    Added Dialog feature

--- src/components/FloatingButton.js
+++ src/components/FloatingButton.js
@@ -2,22 +2,60 @@ import React from 'react';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import ContentClear from 'material-ui/svg-icons/content/clear';
+import TextField from 'material-ui/TextField';
+
 const style = {
   margin: 10,
 };
 
-/**
- * Default size and `mini` FABs, in primary (default), `secondary` and `disabled` colors.
- */
+export default class DialogExampleModal extends React.Component {
+  state = {
+    open: false,
+  };
 
-class FloatingButton extends React.Component {
-    render() {
-        return (<div>
-          <FloatingActionButton style={style} backgroundColor="#512da8">
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Luo lista"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label={<ContentClear />}
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
+    return (
+      <div>
+        <FloatingActionButton label="Modal Dialog" onTouchTap={this.handleOpen}  style={style} backgroundColor="#512da8">
             <ContentAdd />
-          </FloatingActionButton>
-        </div>);
-    }
+        </FloatingActionButton>
+        <Dialog
+          title="Luo uusi lista"
+          actions={actions}
+          modal={true}
+          open={this.state.open}
+        >
+          <TextField 
+            hintText="Listan nimi"
+          />
+        </Dialog>
+      </div>
+    );
+  }
 }
-
-export default FloatingButton;
