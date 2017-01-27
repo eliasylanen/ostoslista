73ad64144c921b6550bf1705307bcbf86986da97
Susanna Kiintonen <Susanna Kiintonen>
2017-01-27 12:22:53 +0200

    Edited list-view button sizes.

--- src/components/DeleteList.js
+++ src/components/DeleteList.js
@@ -3,7 +3,7 @@ import FontIcon from 'material-ui/FontIcon';
 import {grey50} from 'material-ui/styles/colors';
 
 const DeleteList = () => (
-  <FontIcon className="material-icons"color={grey50} style={{float: 'right', cursor: 'pointer'}}>delete</FontIcon>
+  <FontIcon className="material-icons"color={grey50} style={{fontSize: '35px', float: 'right', cursor: 'pointer'}}>delete</FontIcon>
 );
 
 export default DeleteList;
