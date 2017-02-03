1cf71525f8a3e22b3c7e229579467bdb526f871d
Lasse Oksanen <lasse_oksanene@hotmail.com>
2017-02-03 11:45:42 +0200

    Sisältöä listoihin

---
+++ src/components/Card3.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import ImageEdit from 'material-ui/svg-icons/image/edit';
+import {List, ListItem} from 'material-ui/List';
+import ContentClear from 'material-ui/svg-icons/content/clear';
+import SocialPersonAdd from 'material-ui/svg-icons/social/person-add';
+import { Link } from 'react-router';
+
+const styles = {
+  card: {
+    marginTop: 13,
+    backgroundColor: '#fafafa',
+  },
+  icon: {
+    marginLeft: 15,
+    color: '#424242',
+    cursor: 'pointer'
+  },
+  sharedWithText: {
+    fontSize: 12,
+    marginLeft: 14,
+    marginTop: 20,
+    marginBottom: 13,
+    paddingBottom: 9,
+    color: '#838383',
+  },
+  listItemStyle: {
+    fontSize: 14
+  },
+}
+
+const CardContainer = () => (
+  <Card style={styles.card}>
+    <CardHeader
+      title="Susannan lääkkeet"
+      subtitle="Susanna"
+      titleColor="#424242"
+      titleStyle={{fontSize: 16, textTransform: 'uppercase'}}
+      subtitleColor='#838383'
+      actAsExpander={true}
+      showExpandableButton={true}
+    >
+    </CardHeader>
+
+    <CardActions>
+      <ActionDelete style={styles.icon} />
+      <Link to="/lists/asd"><ImageEdit style={styles.icon} /></Link>
+      <SocialPersonAdd style={styles.icon} />
+    </CardActions>
+    <p style={styles.sharedWithText}>Jaettu: <span>Elias, Lasse, Juuso</span></p>
+    <CardText expandable={true}>
+      <List>
+        <ListItem style={styles.listItemStyle} primaryText="Taxi Vodka" leftIcon={<ContentClear style={styles.icon}/>} />
+        <ListItem style={styles.listItemStyle} primaryText="Suolakurkku" leftIcon={<ContentClear style={styles.icon}/>} />
+        <ListItem style={styles.listItemStyle} primaryText="Gambina" leftIcon={<ContentClear style={styles.icon}/>} />
+      </List>
+    </CardText>
+  </Card>
+);
+
+export default CardContainer;
